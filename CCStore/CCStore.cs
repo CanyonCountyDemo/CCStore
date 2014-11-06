9371c90409866603807ca74391571ba5ef514dbd
Kenneth Wilcox <wilcoxk@gmail.com>
2014-11-06 15:22:37 -0700

    Sep 20 2010 code

---
+++ CCStore/CCStore.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// Added Load and Save methods
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CC.Common.Utils
+{
+  // Might be considered lame, but I think it would be easier to use than
+  // the KeyValuePair<object, object> object
+  public class CCStoreItem
+  {
+    public String Name { get; set; }
+    public Object Item { get; set; }
+    public String Comment { get; set; }
+    
+    public CCStoreItem(string name, object item, string comment)
+    {
+      Name = name;
+      Item = item;
+      Comment = comment;
+    }
+  }
+
+  //[Serializable]
+  //internal class CCStoreSerializationHelper : IObjectReference
+  //{
+  //  public object GetRealObject(StreamingContext context)
+  //  {
+  //    return CCStore.Instance();
+  //  }
+  //}
+
+  /// <summary>
+  /// I hate "global" variables. They are usually used to keep track of state. Elsewhere in the program you need
+  /// or would like to check on that state. You either then change the global to public and pass the owner object
+  /// around and each underlying class needs to know about that object (breaking OO encapuslations), or you pass 
+  /// the global variable around and make sure to update it if the value changed for some reason.
+  /// Why do this?
+  /// CCStore - a singleton object that can be used everywhere
+  /// No more passing objects around - this is static
+  /// No more methods with tons of parameters, you won't need any anymore
+  /// No keeping track of state: Read the value, update it if needed
+  /// <code>
+  /// int i = CCStore.Instance().Get("MyVariable", 0);
+  /// // use i
+  /// // increment it
+  /// CCStore.Instance().Store("MyVariable", ++i);
+  /// </code>
+  /// If it's in storage, return that value if not return 0
+  /// Simple
+  /// 
+  /// I'm not sure how thread-safe it is, there is a little check, but not tested
+  /// </summary>
+  [Serializable]
+  public sealed class CCStore: IEnumerable, ISerializable
+  {
+    //private static CCStore instance = null;
+    private static CCStore instance = new CCStore();
+    private static Dictionary<string, object> _items;
+    private static Dictionary<string, string> _comments;
+
+    private readonly object sync = new object();
+
+    private CCStore()
+    {
+      _items = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+      _comments = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+    }
+
+    private CCStore(SerializationInfo info, StreamingContext context)
+    {
+      //info.GetType(typeof(Dictionary<string, object>));
+      //info.GetType(typeof(Dictionary<string, string>));
+      //instance = new CCStore();
+      _items = (Dictionary<string, object>)info.GetValue("_items", typeof(Dictionary<string, object>));
+      _comments = (Dictionary<string, string>)info.GetValue("_comments", typeof(Dictionary<string, string>));
+    }
+
+    //public static CCStore Instance()
+    //{
+    //  if (instance == null)
+    //  {
+    //    instance = new CCStore();
+    //  }
+    //  return instance;
+    //}
+    // Accessor and not a method - preference?
+    public static CCStore Instance
+    {
+      get { return instance; }
+    }
+
+    // TODO - make comment an object so it can be used like so
+    // store.Comment["Key"] = "Value"
+    // comment = store.Comment["Key"]
+    public String this[string name]
+    {
+      get { return _comments[name]; }
+      set { _comments[name] = value; }
+    }
+
+    #region Internal Wrappers
+    private void _store(string name, object item)
+    {
+      lock (sync)
+      {
+        _items[name] = item;
+        _comments[name] = "";
+      }
+    }
+    private object _get(string name, object defaultValue)
+    {
+      lock (sync)
+      {
+        if (_items.ContainsKey(name))
+          return _items[name];
+        else
+          return defaultValue;
+      }
+    }
+    #endregion
+    
+    #region Objects
+    public void Store(string name, object item)
+    {
+      _store(name, item);
+    }
+    public object Get(string name, object defaultValue)
+    {
+      return _get(name, defaultValue);
+    }
+    #endregion
+
+    #region Integers
+    public void Store(string name, int item)
+    {
+      //_items[name] = item;
+      _store(name, item);
+    }
+    public int Get(string name, int defaultValue)
+    {
+      int ret = defaultValue;
+      try
+      {
+        ret = (int)_get(name, defaultValue);
+      }
+      catch { }
+      return ret;
+    }
+    #endregion
+
+    #region Strings
+    public void Store(string name, string item)
+    {
+      //_items[name] = item;
+      _store(name, item);
+    }
+    public string Get(string name, string defaultValue)
+    {
+      string ret = defaultValue;
+      try
+      {
+        ret = (string)_get(name, defaultValue).ToString(); // <<<---- May want to remove this
+      }
+      catch { }
+      return ret;
+    }
+    #endregion
+
+    #region Floats
+    public void Store(string name, double item)
+    {
+      //_items[name] = item;
+      _store(name, item);
+    }
+    public double Get(string name, double defaultValue)
+    {
+      double ret = defaultValue;
+      try
+      {
+        ret = (double)_get(name, defaultValue);
+      }
+      catch { }
+      return ret;
+    }
+    #endregion
+
+    #region DateTimes
+    public void Store(string name, DateTime item)
+    {
+      //_items[name] = item;
+      _store(name, item);
+    }
+    public DateTime Get(string name, DateTime defaultValue)
+    {
+      DateTime ret = defaultValue;
+      try
+      {
+        ret = (DateTime)_get(name, defaultValue);
+      }
+      catch { }
+      return ret;
+    }
+    #endregion
+
+    #region Booleans
+    public void Store(string name, Boolean item)
+    {
+      _store(name, item);
+    }
+    public Boolean Get(string name, Boolean defaultValue)
+    {
+      Boolean ret = defaultValue;
+      try
+      {
+        ret = (Boolean)_get(name, defaultValue);
+      }
+      catch { }
+      return ret;
+    }
+    #endregion
+
+    #region Methods
+    public void Clear()
+    {
+      _items.Clear();
+    }
+
+    public Boolean Delete(string name)
+    {
+      return _items.Remove(name);
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+      foreach (KeyValuePair<string, object> kvp in _items)
+      {
+        yield return new CCStoreItem(kvp.Key, kvp.Value, _comments[kvp.Key]);
+      }
+    }
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+      info.AddValue("_items", _items, typeof(Dictionary<string, object>));
+      info.AddValue("_comments", _comments, typeof(Dictionary<string, string>));
+      //info.SetType(typeof(CCStoreSerializationHelper));
+    }
+
+    public void Save(string filename)
+    {
+      FileStream stream = null;
+      try
+      {
+        IFormatter formatter = new BinaryFormatter();
+        //IFormatter formatter = new XmlFormatter(typeof(CCStore));
+        stream = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite, FileShare.None);
+        //formatter.Serialize(stream, CCStore.Instance());
+        formatter.Serialize(stream, CCStore.Instance);
+        
+        // For compression...
+        //stream.Position = 0; // reset?
+        //FileStream outFile = new FileStream(filename + ".gz", FileMode.Create, FileAccess.Write, FileShare.None);
+        //GZipStream compress = new GZipStream(outFile, CompressionMode.Compress);
+        //byte[] buffer = new byte[4096];
+        //int numRead;
+        //while ((numRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+        //{
+        //  compress.Write(buffer, 0, numRead);
+        //}
+        //compress.Close();
+      }
+      catch
+      {
+        // Do Nothing
+      }
+      finally
+      {
+        if (null != stream)
+          stream.Close();
+      }
+    }
+
+    public void Save(Stream stream)
+    {
+      
+      IFormatter formatter = new BinaryFormatter();
+      formatter.Serialize(stream, CCStore.Instance);
+    }
+
+    public Boolean Load(string filename)
+    {
+      Boolean ret = false;
+      if (File.Exists(filename))
+      {
+        FileStream stream = null;
+        try
+        {
+          IFormatter formatter = new BinaryFormatter();
+          stream = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.None);
+          //instance = null; // Force an update?
+          instance = (CCStore)formatter.Deserialize(stream);
+        }
+        catch
+        {
+          // Do Nothing? toss the exception up - maybe?
+        }
+        finally
+        {
+          if (null != stream)
+            stream.Close();
+        }
+        ret = true;
+      }
+
+      return ret;
+    }
+    
+    public Boolean Load(Stream stream)
+    {
+      Boolean ret;
+      try
+      {
+        IFormatter formatter = new BinaryFormatter();
+        instance = (CCStore)formatter.Deserialize(stream);
+        ret = true;
+      }
+      catch
+      {
+        ret = false;
+      }
+      return ret;
+    }
+    #endregion
+  }
+}
---
+++ CCStore/frmMain.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using CC.Common.Utils;
+using System.IO;
+using System.Drawing.Text;
+
+namespace Test
+{
+  public partial class frmMain : Form
+  {
+    // Why use "globals", stick it in the store
+    //private int counter = 0;
+
+    public frmMain()
+    {
+      CCStore s = CCStore.Instance;
+      if (!s.Load("CCStore.Data.dll"))
+      {
+        s.Store("Name", "Ken Wilcox");
+        s["Name"] = "Someone's name";
+        s.Store("Age", DateTime.Now.AddYears(-1974).Year);
+        s["Age"] = "Someone's Age";
+        s.Store("Date", DateTime.Now);
+        s.Store("Compare", (1 - 2 > 3));
+        s.Store("Counter", 0);
+
+        ArrayList al = new ArrayList();
+        al.Add("One");
+        al.Add("Two");
+        al.Add("Three");
+
+        s.Store("Array", al);
+        s["Array"] = "My ArrayList test";
+
+      }
+      else
+      {
+        this.Size = new Size(s.Get("Width", 0), s.Get("Height", 0));
+        this.StartPosition = FormStartPosition.Manual;
+        this.Location = new Point(s.Get("Left", Left), s.Get("Top", Top));
+        //this.Width = s.Get("Width", 0);
+        //this.Height = s.Get("Height", 0);
+      }
+
+
+      //PrivateFontCollection pfc = new PrivateFontCollection();
+      //pfc.AddFontFile(@"D:\Fonts\Foo.ttf");
+      //pfc.AddFontFile(@"D:\Fonts\ELECTROH.TTF");
+      //pfc.AddFontFile(@"D:\Fonts\Corv2.ttf");
+      //pfc.AddFontFile(@"D:\Fonts\BIRTH OF A HERO.ttf");
+      //this.Font = new Font(pfc.Families[0], 12, FontStyle.Regular);
+
+      InitializeComponent();
+      //MessageBox.Show(s["Array"]);
+      PrintList();
+      //timer1.Enabled = true;
+    }
+
+    private void PrintList()
+    {
+      listBox1.Items.Clear();
+      foreach (CCStoreItem item in CCStore.Instance)
+      {
+        if (String.IsNullOrEmpty(item.Comment))
+          listBox1.Items.Add(item.Name + "=" + item.Item);
+        else
+          listBox1.Items.Add(item.Name + "=" + item.Item + "  // " + item.Comment);
+      }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      CCStore store = CCStore.Instance;
+      store.Store("Name", "John Doe");
+      store["Name"] = "Name changed to John Doe";
+      store.Store("Date", DateTime.Now);
+      store["Date"] = "Date changed to DateTime.Now";
+
+      store.Store("Store", store); // Store a copy - weeee!
+      CCStore q = (CCStore)store.Get("Store", (object)null);
+      PrintList();
+      MessageBox.Show(q.Get("Name", "No Name"));
+      if (store.Delete("Store"))
+        MessageBox.Show("Deleted");
+
+      MessageBox.Show(q.Get("Name", 0).ToString());
+      PrintList();
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      //PrintList();
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      //counter++;
+      CCStore store = CCStore.Instance;
+      int counter = store.Get("Counter", 0);
+      //counter++;
+      // Increment BEFORE we store it, if not we won't go anywhere
+      store.Store("Counter", ++counter);
+      store.Store("Name" + counter.ToString(), "Jane Doe");
+      PrintList();
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      CCStore store = CCStore.Instance;
+      int counter = store.Get("Counter", 0);
+      MessageBox.Show("Name = " + store.Get("Name", "Not Found"));
+      MessageBox.Show("Name"+counter.ToString()+" = " + store.Get("Name" + counter.ToString(), "Not Found"));
+
+      //ArrayList al = (ArrayList)CCStore.Instance().Get("Array", (object)null); // force a null object and not a null string
+      // Never get a null error
+      ArrayList al = (ArrayList)CCStore.Instance.Get("Array", new ArrayList()); // always return a valid object
+      // But you still need to check the length/count/etc
+      if (al.Count > 0)
+      {
+        MessageBox.Show("ArrayList[0] = " + al[0].ToString());
+      }
+      al = null;
+    }
+
+    private void button4_Click(object sender, EventArgs e)
+    {
+      string name = "Name";
+      string item = "Item";
+      InputBoxResult res = InputBox.Show("Enter a Key Name", "Input", name);
+      if (res.OK)
+      {
+        name = res.Text;
+        res = InputBox.Show("Enter a Value", "Input", item);
+        if (res.OK)
+        {
+          item = res.Text;
+
+          CCStore.Instance.Store(name, item);
+          PrintList();
+        }
+      }
+    }
+
+    private void button5_Click(object sender, EventArgs e)
+    {
+      InputBoxResult res = InputBox.Show("What value would you like?", "Input", "Name");
+      if (res.OK)
+      {
+        MessageBox.Show(res.Text + " = " + CCStore.Instance.Get(res.Text, "Sorry, Not Found or Usable in this Context"));
+      }
+    }
+
+    private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+    {
+      CCStore.Instance.Save("CCStore.Data.dll");
+      //MemoryStream ms = new MemoryStream();
+      //CCStore.Instance.Save(ms);
+      //FileStream fs = new FileStream("C:\\DUMMY.INFO", FileMode.Create);
+      //ms.WriteTo(fs);
+    }
+
+    private void listBox1_DoubleClick(object sender, EventArgs e)
+    {
+      // Load/Save to stream test
+      //MemoryStream stream = new MemoryStream();
+      //CCStore.Instance.Save(stream);
+      //stream.Position = 0;
+      //CCStore.Instance.Clear();
+      //PrintList();
+      //MessageBox.Show("All Clear");
+      //CCStore.Instance.Load(stream);
+      //PrintList();
+    }
+
+    private void frmMain_Move(object sender, EventArgs e)
+    {
+      CCStore s = CCStore.Instance;
+      s.Store("Top", Top);
+      s.Store("Left", Left);
+      s.Store("Width", Width);
+      s.Store("Height", Height);
+      PrintList();
+    }
+  }
+}
